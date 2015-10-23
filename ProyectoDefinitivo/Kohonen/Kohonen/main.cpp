e9fccd93a7019eb720d3dd0e29a768328d807cdc
88shell88 <shelley-gill-88@hotmail.com>
2015-10-23 13:06:49 +0200

    añadido peso

--- ProyectoDefinitivo/Kohonen/Kohonen/main.cpp
+++ ProyectoDefinitivo/Kohonen/Kohonen/main.cpp
@@ -19,27 +19,32 @@ int main(const int argc, const char *const *const argv){
 	try{
 		int i = 0;
 		kohonen koh;
-		float input[kohonen::numInput * kohonen::inputSize] = { 53.214, 19.155, 52.349, 18.924,
+		float input[kohonen::numInput * kohonen::inputSize] = { 53.214, 19.155, 52.349, 18.924, //53.214, 19.105, 52.349, 18.924,//
 			53.560, 19.221, 49.763, 19.076,
 			53.280, 19.167, 52.758, 18.790,
 			53.111, 19.220, 52.988, 18.920,
 			54.200, 19.210, 53.719, 19.194,
 			54.390, 19.185, 53.684, 19.031,
-			53.848, 19.079, 54.058, 19.269,
-			53.721, 19.170, 54.076, 19.399,
-			53.603, 19.027, 54.390, 19.399,
+			53.848, 19.079, 54.058, 19.269, //53.948, 19.079, 54.058, 19.269, //
+			53.721, 19.170, 54.076, 19.399, //53.711, 19.070, 54.076, 19.399,//
+			53.603, 19.027, 54.390, 19.399, //53.503, 18.927, 54.390, 19.399,//
 			53.494, 18.969, 49.763, 18.790 };
 		
 		float inputX[kohonen::numInput * kohonen::inputSize / 2];
+		float iX = 0, iY = 0;
 		float inputY[kohonen::numInput * kohonen::inputSize / 2];
 		for (i = 0; i < kohonen::numInput * kohonen::inputSize;i++){
 			if ((i % 2) == 0){
 				inputX[i / 2] = input[i];
+				iX = iX + input[i];
 			}
 			else{
 				inputY[i / 2] = input[i];
+				iY = iY + input[i];
 			}
 		}
+		iX = iX / 20;
+		iY = iY / 20;
 		float maxInputX = *std::max_element(inputX, inputX + kohonen::numInput * kohonen::inputSize/2);
 		float minInputX = *std::min_element(inputX, inputX + kohonen::numInput * kohonen::inputSize/2);
 		float maxInputY = *std::max_element(inputY, inputY + kohonen::numInput * kohonen::inputSize/2);
@@ -49,11 +54,15 @@ int main(const int argc, const char *const *const argv){
 		for (i = 0; i < kohonen::numInput*kohonen::inputSize;i++){
 			if ((i % 2) == 0){
 				//input[i] = (input[i] - minInputX) / (maxInputX - minInputX);
-				input[i] = (input[i] - (minInputX + maxInputX)/2) / (maxInputX - minInputX);
+				input[i] = (input[i] - (minInputX + maxInputX)/2)*1.5 / (maxInputX - minInputX);
+				//input[i] = (input[i] - iX/2) / (maxInputX - minInputX);
+				//printf("------>%f    ,      ", input[i]);
 			}
 			else{
 				//input[i] = (input[i] - minInputY) / (maxInputY - minInputY);
-				input[i] = (input[i] - (minInputY + maxInputY)/2) / (maxInputY - minInputY);
+				input[i] = (input[i] - (minInputY + maxInputY)/2)*1.5 / (maxInputY - minInputY);
+				//input[i] = (input[i] - iY/2) / (maxInputY - minInputY);
+				//printf("%f\n", input[i]);
 			}
 		}
 		ofstream fout("input.txt");
